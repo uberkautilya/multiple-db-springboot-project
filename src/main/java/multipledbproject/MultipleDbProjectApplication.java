e5942b9f10ee0339f719a75f7a52ab53f1392c66
Mithun <mithuncm@gmail.com>
2023-01-18 14:24:48 +0530

    git init

---
+++ src/main/java/multipledbproject/MultipleDbProjectApplication.java
@@ -0,0 +1,13 @@
+package multipledbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/multipledbproject/MultipleDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package multipledbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
